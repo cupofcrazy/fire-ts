218aa0abeba0630ea4a6a1692666e14c2e3002f6
Tobi Balogun <tobybarlhogun@gmail.com>
2021-12-31 18:36:29 -0800

    modified Pin page and abstracted reducer logic

--- src/pages/Pin.tsx
+++ src/pages/Pin.tsx
@@ -1,14 +1,14 @@
 import { deleteDoc, doc, DocumentData, getDoc, collection } from 'firebase/firestore';
 import { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import styled from 'styled-components'
+import Styled from '../styles/pin-page.styles'
 import { db, storage } from '../lib/firebase';
 import { useAuth } from '../contexts/AuthContext';
 import { deleteObject, ref } from 'firebase/storage';
 import { XIcon } from '@heroicons/react/outline';
 import { setDoc } from '@firebase/firestore';
+import { usePinReducer } from '../reducers/usePinReducer';
 
-interface Props {}
 
 const getPinById = async (id: string): Promise<DocumentData | null> => {
   const pinRef = doc(db, 'images', id)
@@ -26,30 +26,47 @@ const getPinById = async (id: string): Promise<DocumentData | null> => {
 const Pin = () => {
   const { user } = useAuth()
   const [pin, setPin] = useState<any>(null)
+  const [state, dispatch] = usePinReducer()
+  const [loading, setLoading] = useState(true)
   const params = useParams()
   const navigate = useNavigate()
 
+  // Save Pin
   const savePin = async () => {
+    dispatch({
+      type: 'SAVE_PIN'
+    })
+    console.log('saving pin...')
     const ref = doc(db, `users/${user?.uid!}/saves`, pin.id)
     await setDoc(ref, {
       id: pin.id
     }, {
       merge: true
     })
+    // disable save button if pin is already saved
+    try {
+      await disablePin(pin.id)
+      dispatch({
+        type: 'PIN_SAVED',
+        payload: true
+      })
+ 
+    } catch (e) {
+      console.log({ e })
+    }
   }
 
-  const disablePin = (id: string) => {
+  // Disable "Save Pin" if already saved
+  const disablePin = async (id: string) => {
     const pinRef = doc(db, `users/${user?.uid!}/saves`, id)
-    getDoc(pinRef).then(pinSnapshot => {
-      if (pinSnapshot.exists()) {
-        return true
-      }
-
-    })
-
-    return true
+    const pinSnapshot = await getDoc(pinRef)
+    if (pinSnapshot.exists()) {
+      return true
+    }
+    return false
   }
 
+  // Delete pin if user's
   const deletePin = async (id: string) => {
     const pinRef = ref(storage, `images/${user?.username}/${pin.name}`)
     try {
@@ -63,97 +80,46 @@ const Pin = () => {
 
   }
 
+  // Intial Page Load and checks
   useEffect(() => {
     getPinById(params.id!).then(data => {
       setPin(data)
+      disablePin(params.id!).then((bool) => {
+        dispatch({
+          type: 'CHECK_IF_PIN_SAVED',
+          payload: bool
+        })
+        setLoading(false)
+      })
     })
     console.log(pin)
-  }, [])
+  }, [state.isSaved, params.id])
+
+  // Render
   return (
-    <StyledPage color={'#fff'}>
-      <StyledHeader>
-        <div className="pin-info">
-          <h3>{ pin?.id }</h3>
-          <span></span>
-          <Link to={'/'}>
-            <StyledImage src={pin?.user.photoURL} alt="Image" />
-          </Link>
-        </div>
-        <div className="pin-options">
-          <button onClick={savePin}>Save</button>
-          <button>Show Details</button>
-          { user?.uid === pin?.user?.uid && <button onClick={ () => deletePin(pin?.id)}>Delete</button> }
-          <button aria-label='Close' title="Close" onClick={() => navigate('/')}><XIcon width={24} height={24} /></button>
-        </div>
-      </StyledHeader>
-      <StyledImage src={pin?.image} style={{ width: '50%' }} alt={pin?.id} />
-    </StyledPage>
+    <Styled.Page color={'#fff'}>
+      { loading ? <p style={{ fontSize: '20vw' }}>Loading...</p> : 
+      <>
+        <Styled.Header>
+          <div className="pin-info">
+            <h3>{ pin?.id }</h3>
+            <span></span>
+            <Link to={'/'}>
+              <Styled.Image src={pin?.user?.photoURL} alt="Image" />
+            </Link>
+          </div>
+          <div className="pin-options">
+            <button onClick={savePin} disabled={state.isSaved || state.isSaving}>{ state.isSaved ? 'Saved' : state.isSaving ? 'Saving...' : 'Save' }</button>
+            <button>Show Details</button>
+            { user?.uid === pin?.user?.uid && <button onClick={ () => deletePin(pin?.id)}>Delete</button> }
+            <button aria-label='Close' title="Close" onClick={() => navigate('/')}><XIcon width={24} height={24} /></button>
+          </div>
+        </Styled.Header>
+        <Styled.Image src={pin?.image} style={{ width: '50%' }} alt={pin?.id} />
+      </>
+      }
+    </Styled.Page>
   )
 }
 
 export default Pin
-
-const StyledPage = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: ${({ color }) => color || '#eee' };
-  padding: 1rem;
-  overflow-y: scroll;
-`
-
-const StyledImage = styled.img`
-  margin: 1rem auto;
-`
-
-const StyledHeader = styled.header`
-  width: 100%;
-  /* padding: 2rem 0 1rem 0; */
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-bottom: 1px solid rgba(0, 0, 0, .05);
-  
-  .pin-info {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  .pin-options {
-    display: flex;
-    gap: 1rem;
-  }
-
-  button {
-    background-color: transparent;
-    color: #494949;
-    padding: .75rem 1.25rem;
-    border-radius: .5rem;
-    
-    &:focus, &:hover {
-      background-color: rgba(0, 0, 0, .05);
-
-    }
-
-    &::disabled {
-      color: red;
-    }
-  }
-
-  span {
-    display: block;
-    width: .5rem;
-    height: .5rem;
-    border-radius: 10rem;
-    background-color: #111;
-  }
-
-  img {
-    width: 2rem;
-    height: 2rem;
-    border-radius: 10rem;
-  }
-`
